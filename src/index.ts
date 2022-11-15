908ade19d74499a48efe66c627682664babf8efd
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-14 23:17:39 -0300

    feat: setup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+import path from 'node:path';
 
-const app = express();
+import { router } from './router';
 
-const port = process.env.PORT || 3001;
+dotenv.config();
 
-app.listen(port, () => {
-  console.log(`🚀 HTTP server running on port ${port}`);
-});
+mongoose
+  .connect(String(process.env.DATABASE_URL))
+  .then(() => {
+    const app = express();
+    const port = process.env.PORT || 3001;
+
+    app.use(cors());
+    app.use(express.json());
+    app.use(
+      '/uploads',
+      express.static(path.resolve(__dirname, '..', 'uploads'))
+    );
+
+    app.use(router);
+
+    app.listen(port, () => {
+      console.log(`🚀 HTTP server running on port ${port}`);
+    });
+  })
+  .catch(() => console.log('Failed to connect to database! 😥'));
