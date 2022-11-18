79b129d16476645bfce7d1d2c7d7ac8539ac46c1
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-18 14:41:45 -0300

    feat: add main component

---
+++ mobile/src/components/Main/styles.ts
@@ -0,0 +1,28 @@
+import { Platform, StatusBar } from 'react-native';
+import styled from 'styled-components/native';
+
+const isAndroid = Platform.OS === 'android';
+
+export const Container = styled.SafeAreaView`
+  margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : 0};
+  flex: 1;
+  background: #fafafa;
+`;
+
+export const CategoriesContainer = styled.View`
+  height: 72px;
+  margin-top: 32px;
+`;
+
+export const MenuContainer = styled.View`
+  flex: 1;
+  height: 30px;
+`;
+
+export const Footer = styled.View`
+  min-height: 110px;
+  background: #fff;
+  padding: 16px 24px;
+`;
+
+export const FooterContainer = styled.SafeAreaView``;
