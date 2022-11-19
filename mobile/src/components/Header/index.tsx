86269253a3119e4ac473f7f8566e148ce9709a4c
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-19 17:45:55 -0300

    feat: show table on header

--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -1,16 +1,45 @@
-import { Text } from '../Text';
-import { Container } from './styles';
+import { TouchableOpacity } from 'react-native';
 
-export function Header() {
+import { Text } from '../Text';
+import { Container, Order, OrderHeader, Table } from './styles';
+
+interface Props {
+  selectedTable?: string;
+  onCancelOrder: () => void;
+}
+
+export function Header({ selectedTable, onCancelOrder }: Props) {
   return (
     <Container>
-      <Text size={14} opacity={0.9}>
-        Bem-vindo(a) ao
-      </Text>
-      <Text size={24} weight={700}>
-        WAITER
-        <Text size={24}>APP</Text>
-      </Text>
+      {!selectedTable && (
+        <>
+          <Text size={14} opacity={0.9}>
+            Bem-vindo(a) ao
+          </Text>
+          <Text size={24} weight={700}>
+            WAITER
+            <Text size={24}>APP</Text>
+          </Text>
+        </>
+      )}
+      {selectedTable && (
+        <Order>
+          <OrderHeader>
+            <Text size={24} weight={600}>
+              Pedido
+            </Text>
+            <TouchableOpacity onPress={onCancelOrder}>
+              <Text color='#d73035' weight={600} size={14}>
+                Cancelar Pedido
+              </Text>
+            </TouchableOpacity>
+          </OrderHeader>
+
+          <Table>
+            <Text color='#666'>Mesa {selectedTable}</Text>
+          </Table>
+        </Order>
+      )}
     </Container>
   );
 }
