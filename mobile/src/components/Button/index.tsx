53b88a7fe3e6765d1ed7105e7cdde47a439eeb00
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-20 11:51:04 -0300

    feat: add loading state

--- mobile/src/components/Button/index.tsx
+++ mobile/src/components/Button/index.tsx
@@ -1,16 +1,26 @@
-import { TouchableOpacityProps } from 'react-native';
+import { ActivityIndicator, TouchableOpacityProps } from 'react-native';
 
 import { Text } from '../Text';
 import { Container } from './styles';
 
-interface Props extends TouchableOpacityProps {}
+interface Props extends TouchableOpacityProps {
+  loading?: boolean;
+}
 
-export function Button({ children, ...props }: Props) {
+export function Button({
+  children,
+  disabled,
+  loading = false,
+  ...props
+}: Props) {
   return (
-    <Container {...props}>
-      <Text weight={600} color='#fff'>
-        {children}
-      </Text>
+    <Container disabled={disabled || loading} {...props}>
+      {!loading && (
+        <Text weight={600} color='#fff'>
+          {children}
+        </Text>
+      )}
+      {loading && <ActivityIndicator color='#fff' />}
     </Container>
   );
 }
