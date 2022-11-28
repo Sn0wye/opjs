cffad1637aa7676b74acc44369f171f8503239fe
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-28 15:04:08 -0300

    feat: cancel and change status

--- frontend/src/components/OrdersBoard/index.tsx
+++ frontend/src/components/OrdersBoard/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 
+import { api } from '../../services/api';
 import { Order } from '../../types/Order';
 import { OrderModal } from '../OrderModal';
 import { Board, OrderContainer } from './styles';
@@ -8,11 +10,20 @@ interface Props {
   icon: string;
   title: string;
   orders: Order[];
+  onCancel: (orderId: string) => void;
+  onStatusChange: (orderId: string, status: Order['status']) => void;
 }
 
-export const OrderBoard = ({ title, icon, orders }: Props) => {
+export const OrderBoard = ({
+  title,
+  icon,
+  orders,
+  onCancel,
+  onStatusChange
+}: Props) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const toggleModal = () => {
     setIsModalOpen(state => !state);
@@ -23,12 +34,45 @@ export const OrderBoard = ({ title, icon, orders }: Props) => {
     toggleModal();
   };
 
+  const handleCancelOrder = async () => {
+    if (!selectedOrder) return;
+    setIsLoading(true);
+
+    await api.delete(`/orders/${selectedOrder._id}`);
+    toast.success(`O pedido da Mesa ${selectedOrder.table} foi cancelado`);
+
+    onCancel(selectedOrder._id);
+    setIsLoading(false);
+    setIsModalOpen(false);
+  };
+
+  const handleStatusChange = () => {
+    if (!selectedOrder) return;
+
+    const status =
+      selectedOrder.status === 'WAITING' ? 'IN_PRODUCTION' : 'DONE';
+
+    setIsLoading(true);
+    api.patch(`/orders/${selectedOrder._id}`, { status });
+
+    toast.success(
+      `O pedido da mesa ${selectedOrder.table} teve o status alterado!`
+    );
+
+    onStatusChange(selectedOrder._id, status);
+    setIsLoading(false);
+    setIsModalOpen(false);
+  };
+
   return (
     <Board>
       <OrderModal
         isOpen={isModalOpen}
         toggleModal={toggleModal}
         order={selectedOrder}
+        onCancel={handleCancelOrder}
+        isLoading={isLoading}
+        onStatusChange={handleStatusChange}
       />
 
       <header>
