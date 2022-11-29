fffdb69450ae7c87345ac42eacc1442fc234d4a1
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-29 14:42:42 -0300

    feat: call api from the hook

--- frontend/src/hooks/useOrder.ts
+++ frontend/src/hooks/useOrder.ts
@@ -1,28 +1,41 @@
+import { toast } from 'react-toastify';
 import create from 'zustand';
 
+import { api } from '../services/api';
 import { Order } from '../types/Order';
 
 interface OrderContext {
   orders: Order[];
   setOrders: (orders: Order[]) => void;
-  cancelOrder: (orderId: string) => void;
-  changeOrderStatus: (orderId: string, status: Order['status']) => void;
+  cancelOrder: (order: Order) => Promise<void>;
+  changeOrderStatus: (order: Order, status: Order['status']) => Promise<void>;
   addOrder: (order: Order) => void;
 }
 
 export const useOrder = create<OrderContext>((set, get) => ({
   orders: [],
-  setOrders: orders => set(state => ({ orders })),
-  changeOrderStatus: (orderId, status) =>
+  setOrders: orders => set({ orders }),
+  changeOrderStatus: async (order, status) => {
+    const orderId = order._id;
+    api.patch(`/orders/${orderId}`, { status });
+
+    toast.success(`O pedido da mesa ${order.table} teve o status alterado!`);
+
     set(state => ({
       ...state,
       orders: state.orders.map(order =>
         order._id === orderId ? { ...order, status } : order
       )
-    })),
-  cancelOrder: orderId =>
+    }));
+  },
+  cancelOrder: async order => {
+    const orderId = order._id;
+    await api.delete(`/orders/${orderId}`);
+    toast.success(`O pedido da Mesa ${order.table} foi cancelado`);
+
     set(state => ({
       orders: state.orders.filter(order => order._id !== orderId)
-    })),
+    }));
+  },
   addOrder: order => set(state => ({ orders: [...state.orders, order] }))
 }));
